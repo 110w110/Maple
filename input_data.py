f57f7288cb97b130819b64a199b1604c77e2e481
110w110 <110w110.official@gmail.com>
2022-05-15 17:28:43 +0900

    input_data.py 수정

--- input_data.py
+++ input_data.py
@@ -42,3 +42,50 @@ def full_year(year):
 
     return int(year)
 
+
+def get_cell_data(file, sheetname ,cell):
+    # print(sheetname)
+    sheet = file.get_sheet_by_name(sheetname)
+    return sheet[cell].value
+
+def get_singleline_data(file, sheetname, firstcell, lastcell):
+    cell = firstcell
+    data = []
+
+    if firstcell[1:] != lastcell[1:]:
+        if 65 <= ord(firstcell[1]) <= 90:
+            if firstcell[2:] != lastcell[2:]:
+                print("error")
+                return data
+        else:
+            print("error")
+            return data
+
+    while cell != lastcell:
+        data.append(get_cell_data(file,sheetname,cell))
+        if 65 <= ord(cell[1]) <= 90:
+            cell = cell[0] + chr(ord(cell[1])+1) + cell[2:]
+        else:
+            cell = chr(ord(cell[:1])+1) + cell[1:]
+    return data
+
+def get_single_column(file, sheetname, firstcell, lastcell):
+    cell = firstcell
+    data = []
+
+    if firstcell[:1] != lastcell[:1]:
+        print("error")
+        return data
+
+    while cell != lastcell:
+        data.append(get_cell_data(file,sheetname,cell))
+        cell = cell[:1]+str(int(cell[1:])+1)
+    return data
+
+# 테스트용
+# if __name__ == '__main__':
+#     file = load_xls('temperatures.xlsx')
+#     print(get_cell_data(file,'기온','A2'))
+#     print(get_singleline_data(file,'기온','A2','D2'))
+#     print(get_single_column(file,'기온','B2','B14'))
+
