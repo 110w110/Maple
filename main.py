9a044e2ff34a79f09311a20783c3fea4f21a4ca5
squirMM <jsm6616@naver.com>
2022-05-25 22:58:20 +0900

    add batch normalization

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import time
-
 import keras.initializers
 from tensorflow.keras import models
 from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import BatchNormalization, Dense, Activation
 from tensorflow.keras import optimizers
 from input_data import *
 import numpy as np
@@ -15,8 +16,9 @@ keras.initializers.he_normal(seed=None)
 file = load_xls('./data/maple.xlsx')
 start_date = get_single_column(file, 'sheet1', 'B2', 'B35')
 for i in range(len(start_date)):
+    # TODO:날짜 파싱
     start_date[i] = (int(start_date[i].strftime("%Y%m%d")[4:6])-10)*31 + int(start_date[i].strftime("%Y%m%d")[6:])
-    # start_date[i] = float(start_date[i].strftime("%Y%m%d")[6:])
+
 min_temper = get_single_column(file, 'sheet2', 'D14', 'D408')
 max_temper = get_single_column(file, 'sheet2', 'E14', 'E408')
 # rain_weight = get_single_column(file, 'sheet3', 'C2', 'C408')
@@ -46,37 +48,44 @@ y = np.array(start_date)
 # y = y / y.max()
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=False, random_state=1004)
-# x = np.array([1,2,3,4,5])
-# y = np.array([2,4,6,8,10])
 print('x : ',x_test)
 print('y : ',y_test)
 
-
-model = models.Sequential()
-# model.add(layers.Dense(10, input_shape=(1,)))
-# model.add(layers.Dense(1))
-
-# model.add(layers.Dense(1, input_dim=1))
+# # TODO: 정규화
+# mean = x_train.mean(axis=0)
+# x_train -= mean
+# std = x_train.std(axis=0)
+# x_train /= std
 #
+# x_test -= mean
+# x_test /= std
 
-# model.add(layers.Dense(32, activation='relu'))
-# model.add(layers.Dense(3, activation='relu'))
-model.add(layers.Dense(32, activation='softmax'))
-# model.add(layers.Dense(units=33,activation='relu'))
-# model.add(layers.Dense(units=33,activation='relu'))
-# model.add(layers.Dense(units=33,activation='sigmoid'))
-# sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model = Sequential()
 
-# model.compile("sgd", "mse")
-model.compile(optimizer='adam',loss='mse',metrics=['accuracy'])
+# TODO: hidden layer 추가?
+#model.add(Dense(units=3,activation='softmax'))
+
+# TODO: batch_normalization
+model.add(Dense(5, kernel_initializer='he_normal'))
+# model.add(layers.Dense(3))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+
+#sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+#sgd = optimizers.SGD(lr=0.01,decay=1e-7)
+sgd = optimizers.SGD(lr=0.01)
+
+#TODO: 일반 사용
+#model.compile(optimizer='adam',loss='mse',metrics=['accuracy'])
+
+#TODO: sgd 직접 설정
+model.compile(optimizer=sgd,loss='mse',metrics=['accuracy'])
+
 # model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
 # model.compile(loss='mean_squared_error', optimizer='sgd')
-# model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-
-# model.fit(x,y, epochs=1000)
-
-model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=1000)
+# TODO: validation split / Test 에서 val 구하면 안돼
+model.fit(x_train, y_train, validation_split=0.1, epochs=1000)
 print(model.summary())
 
 test_loss, test_acc = model.evaluate(x_test, y_test)
@@ -90,14 +99,6 @@ predictions = model.predict(x_test)
 print('y_test = ',y_test[:10])
 print('predictions = ', np.argmax(predictions[:10],axis=1))
 
-# model.add(layers.Dense(units=5,kernel_initializer='normal',activation='sigmoid'))
-#
-#
-# model.compile(loss='categorical_crossentropy',
-#               optimizer='sgd',
-#               metrics=['accuracy'])
-# model.fit(x,y, epochs=5)
-
 # for i in range(33):
 #     model.fit(x[i*10:i*10+10], y[i], epochs=1000, verbose=1)
 
@@ -105,76 +106,6 @@ print('predictions = ', np.argmax(predictions[:10],axis=1))
 # print("predict",*predict)
 # print("real",y[32])
 
-
 # print(hist.history['loss'])
 # print(hist.history['acc'])
 # print(hist.history['val_loss'])
-
-
-
-# model = models.Sequential()
-# model.add(layers.Dense(32, activation='relu', input_shape=(23,)))
-# model.add(layers.Dense(10, activation='softmax'))
-#
-# model.compile(optimizer=optimizers.RMSprop(lr=0.001), loss='mse')
-#
-# model.fit(input_tensor, target_tensor, batch_size=128, epochs=10)
-#
-# print(model)
-
-# output = None
-#
-# hidden_layer_weights = tf.Variable(tf.random.normal([4,3]))
-# out_weights = tf.Variable(tf.random.normal([3,2]))
-#
-# # Weights and biases
-# weights = [
-#     tf.Variable(hidden_layer_weights),
-#     tf.Variable(out_weights)]
-# biases = [
-#     tf.Variable(tf.zeros(3)),
-#     tf.Variable(tf.zeros(2))]
-#
-# # Input TEST
-# pre = [15.0, 30.0, 50.0, 100.0]
-# temp = [10.0, 20.0, 30.0, 40.0]
-# dust = [1.0, 2.0, 20.0, 35.0]
-#
-# features = tf.Variable([pre, temp, dust])
-#
-# # TODO: 레이어 추가 필요
-# hidden_layer = tf.add(tf.matmul(features, weights[0]), biases[0])
-# hidden_layer = tf.nn.relu(hidden_layer)
-# logits = tf.add(tf.matmul(hidden_layer, weights[1]), biases[1])
-#
-# print(logits)
-
-
-
-# # tf Graph input
-# x = tf.placeholder("float", [None, 28, 28, 1])
-# y = tf.placeholder("float", [None, n_classes])
-#
-# x_flat = tf.reshape(x, [-1, n_input])
-
-
-# Define loss and optimizer
-# cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
-# optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)\
-#     .minimize(cost)
-#
-# # Initializing the variables
-# init = tf.global_variables_initializer()
-#
-# # Launch the graph
-# with tf.Session() as sess:
-#     sess.run(init)
-#     # Training cycle
-#     for epoch in range(training_epochs):
-#         total_batch = int(mnist.train.num_examples / batch_size)
-#         # Loop over all batches
-#         for i in range(total_batch):
-#             batch_x, batch_y = mnist.train.next_batch(batch_size)
-#             # Run optimization op (backprop) and cost op (to get loss value)
-#             sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
-#
