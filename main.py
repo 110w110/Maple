76d2e80a0afb30d7a5b0ff70e104aa323825036a
110w110 <110w110.official@gmail.com>
2022-05-25 11:19:37 +0900

    0525 0.07

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import time
 
+import keras.initializers
 from tensorflow.keras import models
 from tensorflow.keras import layers
 from tensorflow.keras import optimizers
 from input_data import *
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 
-
+keras.initializers.he_normal(seed=None)
 # x = np.array([0,1,2,3,4])
 # y = x * 2 + 1
 file = load_xls('./data/maple.xlsx')
-start_date = get_single_column(file, '단풍시기', 'B2', 'B35')
+start_date = get_single_column(file, 'sheet1', 'B2', 'B35')
 for i in range(len(start_date)):
     start_date[i] = (int(start_date[i].strftime("%Y%m%d")[4:6])-10)*31 + int(start_date[i].strftime("%Y%m%d")[6:])
     # start_date[i] = float(start_date[i].strftime("%Y%m%d")[6:])
-min_temper = get_single_column(file, '기온', 'D14', 'D408')
-max_temper = get_single_column(file, '기온', 'E14', 'E408')
-rain_weight = get_single_column(file, 'rainw', 'C2', 'C408')
-rain_time = get_single_column(file, 'rainm', 'C2', 'C35')
+min_temper = get_single_column(file, 'sheet2', 'D14', 'D408')
+max_temper = get_single_column(file, 'sheet2', 'E14', 'E408')
+# rain_weight = get_single_column(file, 'sheet3', 'C2', 'C408')
+# rain_time = get_single_column(file, 'sheet3', 'C2', 'C35')
 for i in range(393, -1, -1):
     if 0 <= i % 12 < 5 or 10 <= i % 12:
         del (min_temper[i])
         del (max_temper[i])
-        del (rain_weight[i])
+        # del (rain_weight[i])
 np.array(min_temper[i]).astype(float)
 np.array(max_temper[i]).astype(float)
 x = []
@@ -33,14 +35,22 @@ for i in range(33):
         v.append(min_temper[i*5+j])
     for j in range(5):
         v.append(max_temper[i*5+j])
-    for j in range(5):
-        v.append(rain_weight[i*5+j])
+    # for j in range(5):
+    #     v.append(rain_weight[i*5+j])
     x.append(v)
 
 x = np.array(x)
 y = np.array(start_date)
+
+# x = x / x.max()
+# y = y / y.max()
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=False, random_state=1004)
 # x = np.array([1,2,3,4,5])
 # y = np.array([2,4,6,8,10])
+print('x : ',x_test)
+print('y : ',y_test)
+
 
 model = models.Sequential()
 # model.add(layers.Dense(10, input_shape=(1,)))
@@ -48,18 +58,37 @@ model = models.Sequential()
 
 # model.add(layers.Dense(1, input_dim=1))
 #
-model.add(layers.Dense(33, activation='relu'))
+
+# model.add(layers.Dense(32, activation='relu'))
 # model.add(layers.Dense(3, activation='relu'))
-model.add(layers.Dense(2, activation='softmax'))
+model.add(layers.Dense(32, activation='softmax'))
 # model.add(layers.Dense(units=33,activation='relu'))
 # model.add(layers.Dense(units=33,activation='relu'))
 # model.add(layers.Dense(units=33,activation='sigmoid'))
 # sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 
-model.compile("sgd", "mse")
+# model.compile("sgd", "mse")
+model.compile(optimizer='adam',loss='mse',metrics=['accuracy'])
+# model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
 # model.compile(loss='mean_squared_error', optimizer='sgd')
 # model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-model.fit(x,y, epochs=1000)
+
+
+# model.fit(x,y, epochs=1000)
+
+model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=1000)
+print(model.summary())
+
+test_loss, test_acc = model.evaluate(x_test, y_test)
+
+print('test_loss : ',test_loss)
+print('test_acc : ',test_acc)
+print()
+
+predictions = model.predict(x_test)
+
+print('y_test = ',y_test[:10])
+print('predictions = ', np.argmax(predictions[:10],axis=1))
 
 # model.add(layers.Dense(units=5,kernel_initializer='normal',activation='sigmoid'))
 #
