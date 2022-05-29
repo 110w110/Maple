ec44256489fafcb13664d76b5ae44d5ed23fe5f2
squirMM <jsm6616@naver.com>
2022-05-29 20:54:19 +0900

    shit~

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import time
 
 import keras.initializers
-from tensorflow.keras import models
-from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import BatchNormalization, Dense, Activation
 from tensorflow.keras import optimizers
@@ -11,35 +9,30 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 keras.initializers.he_normal(seed=None)
-# x = np.array([0,1,2,3,4])
-# y = x * 2 + 1
+
 file = load_xls('./data/maple.xlsx')
-start_date = get_single_column(file, 'sheet1', 'B2', 'B35')
-forsythia_flowering = get_single_column(file, 'sheet6', 'D2', 'D35')
+start_date = get_single_column(file, 'sheet1', 'B2', 'B99')
+forsythia_flowering = get_single_column(file, 'sheet6', 'D2', 'D99')
 for i in range(len(start_date)):
     # TODO:날짜 파싱
     start_date[i] = (int(start_date[i].strftime("%Y%m%d")[4:6])-10)*31 + int(start_date[i].strftime("%Y%m%d")[6:])
     forsythia_flowering[i] = (int(forsythia_flowering[i].strftime("%Y%m%d")[4:6])-3)*31 + int(forsythia_flowering[i].strftime("%Y%m%d")[6:])
-    # start_date[i] = float(start_date[i].strftime("%Y%m%d")[6:])
-min_temper = get_single_column(file, 'sheet2', 'D14', 'D408')
-max_temper = get_single_column(file, 'sheet2', 'E14', 'E408')
-#rain_weight = get_single_column(file, 'sheet3', 'B2', 'B35')
-rain_weight = get_single_column(file, 'sheet4', 'C2', 'C409')
-sun_summer = get_singleline_data(file, 'sheet7', 'B6', 'AI6')
-sun_fall = get_singleline_data(file, 'sheet7', 'B7', 'AI7')
-# rain_time = get_single_column(file, 'sheet3', 'C2', 'C35')
+
+min_temper = get_single_column(file, 'sheet2', 'D14', 'D919')
+max_temper = get_single_column(file, 'sheet2', 'E14', 'E919')
+
+rain_weight = get_single_column(file, 'sheet4', 'C2', 'C1597')
+sun_summer = get_singleline_data(file, 'sheet7', 'B6', 'CU6')
+sun_fall = get_singleline_data(file, 'sheet7', 'B7', 'CU7')
+
 for i in range(393, -1, -1):
     if 0 <= i % 12 < 7 or 9 <= i % 12:
         del (min_temper[i])
         del (max_temper[i])
         del (rain_weight[i])
-        # del (rain_weight[i])
-np.array(min_temper[i]).astype(float)
-np.array(max_temper[i]).astype(float)
-np.array(rain_weight[i]).astype(float)
 
 x = []
-for i in range(33):
+for i in range(97):
     v = []
     for j in range(2):
         v.append(min_temper[i*2+j])
@@ -56,34 +49,14 @@ for i in range(33):
 x = np.array(x)
 y = np.array(start_date)
 
-#print(x)
-# x = x / x.max()
-# y = y / y.max()
-
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, shuffle=False, random_state=1004)
-# print('x_test : ', x_test)
-# print('y_test : ', y_test)
-print('x_train : ', x_train)
-print('y_train : ', y_train)
-
-# train_size = len(y_train)
-# test_size = len(y_test)
-# print(train_size, test_size)
-
-# # TODO: 정규화
-# mean = x_train.mean(axis=0)
-# x_train -= mean
-# std = x_train.std(axis=0)
-# x_train /= std
-#
-# x_test -= mean
-# x_test /= std
+# print('x_train : ', x_train)
+# print('y_train : ', y_train)
 
 model = Sequential()
 
 # TODO: batch_normalization
 model.add(Dense(32, kernel_initializer='he_normal'))
-# model.add(layers.Dense(3))
 model.add(BatchNormalization())
 model.add(Activation('relu'))
 
@@ -103,9 +76,8 @@ model.compile(optimizer=sgd,loss='mse',metrics=['accuracy'])
 # model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
 # model.compile(loss='mean_squared_error', optimizer='sgd')
 
-# TODO: validation split / Test 에서 val 구하면 안돼
-model.fit(x_train, y_train, validation_split=0.1, epochs=1000)
-# model.fit(x_train, y_train, epochs=1000)
+# TODO: validation split
+model.fit(x_train, y_train, validation_split=0.2, epochs=1000)
 print(model.summary())
 
 test_loss, test_acc = model.evaluate(x_test, y_test)
@@ -116,16 +88,19 @@ print()
 
 predictions = model.predict(x_test)
 
-print('y_test = ',y_test[:10])
-print('predictions = ', np.argmax(predictions[:10],axis=1))
+# print('y_test = ',y_test[:10])
+# print('predictions = ', np.argmax(predictions[:10],axis=1))
 
-# for i in range(33):
-#     model.fit(x[i*10:i*10+10], y[i], epochs=1000, verbose=1)
+predictions=np.argmax(predictions[:10],axis=1)
 
-# predict = model.predict(x[0][32])
-# print("predict",*predict)
-# print("real",y[32])
+def itd(num):
+    month = 10
+    if num > 31:
+        month += 1
+        num -= 31
+    return str(month)+"/"+str(num)
 
-# print(hist.history['loss'])
-# print(hist.history['acc'])
-# print(hist.history['val_loss'])
+print("Data\tPrediction\tReal\tDifference")
+for i in range(len(y_test)):
+    print(i + 1, "th \t", itd(predictions[i]), "    \t", itd(y_test[i]), sep='', end='')
+    print(" \t", abs(predictions[i] - y_test[i]), " day", sep='')
