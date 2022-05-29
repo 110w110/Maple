8d621fe7b3fd8c5b2cafac7d5c817696c7da5854
110w110 <110w110.official@gmail.com>
2022-05-29 17:57:23 +0900

    dfghj

--- temp.py
+++ temp.py
@@ -1,8 +1,8 @@
-from input_data import *
-import numpy as np
 import sys
+import numpy as np
 from sklearn.model_selection import train_test_split
 from knn import Knn
+from input_data import *
 
 def normalize(arr):
     normalized = [[] for _ in range(len(arr))]
@@ -25,18 +25,18 @@ def itd(num):
     return str(month)+"/"+str(num)
 
 file = load_xls('./data/maple.xlsx')
-start_date = get_single_column(file, 'sheet1', 'B2', 'B99')
-forsythia_flowering = get_single_column(file, 'sheet6', 'D2', 'D99')
+start_date = get_single_column(file, 'sheet1', 'B2', 'B35')
+forsythia_flowering = get_single_column(file, 'sheet6', 'D2', 'D35')
 for i in range(len(start_date)):
     # TODO:날짜 파싱
     start_date[i] = (int(start_date[i].strftime("%Y%m%d")[4:6])-10)*31 + int(start_date[i].strftime("%Y%m%d")[6:])
     forsythia_flowering[i] = (int(forsythia_flowering[i].strftime("%Y%m%d")[4:6])-3)*31 + int(forsythia_flowering[i].strftime("%Y%m%d")[6:])
 
-min_temper = get_single_column(file, 'sheet2', 'D14', 'D919')
-max_temper = get_single_column(file, 'sheet2', 'E14', 'E919')
-rain_weight = get_single_column(file, 'sheet3', 'B2', 'B99')
-sun_summer = get_singleline_data(file, 'sheet7', 'B6', 'CU6')
-sun_fall = get_singleline_data(file, 'sheet7', 'B7', 'CU7')
+min_temper = get_single_column(file, 'sheet2', 'D14', 'D408')
+max_temper = get_single_column(file, 'sheet2', 'E14', 'E408')
+rain_weight = get_single_column(file, 'sheet3', 'B2', 'B35')
+sun_summer = get_singleline_data(file, 'sheet7', 'B6', 'AI6')
+sun_fall = get_singleline_data(file, 'sheet7', 'B7', 'AI7')
 
 for i in range(393, -1, -1):
     if 0 <= i % 12 < 5 or 10 <= i % 12:
@@ -44,7 +44,7 @@ for i in range(393, -1, -1):
         del (max_temper[i])
 
 x = []
-for i in range(97):
+for i in range(33):
     v = []
     for j in range(5):
         v.append(min_temper[i*5+j])
@@ -58,7 +58,6 @@ for i in range(97):
 
 x = np.array(normalize(x))
 y = np.array(start_date)
-print(start_date)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.17, shuffle=True)
 
@@ -72,8 +71,8 @@ acc = 0
 print("Data\tPrediction\tReal\tDifference")
 for i in range(test_size):
     result = k1.neighbor(k, k1.distance(i))
-    print(i + 1, "th \t", itd(result), "    \t", itd(y_test[i]), sep='', end='')
-    print(" \t", abs(result - y_test[i]), " day", sep='')
+    print(i+1, "th \t", itd(result), "    \t", itd(y_test[i]), sep='', end='')
+    print(" \t",abs(result - y_test[i])," day",sep='')
 
     # acc += 1 - abs(result - y_test[i])/result
     # print(", Accuracy : %.2f" % ((1-abs(result-y_test[i])/result)*100), "%", sep='')
